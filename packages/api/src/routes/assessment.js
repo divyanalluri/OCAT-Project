bb33ba209fa3fe71de0aca8e49a0cf1f8be86859
Divya Nalluri <divyanalluri@divyas-air.wifi.uc.edu>
2023-03-16 14:42:38 -0400

    Task 2 completed - View Assessment List

--- packages/api/src/routes/assessment.js
+++ packages/api/src/routes/assessment.js
@@ -27,13 +27,12 @@ assessmentRouter.post(
 );
 
 assessmentRouter.get(
-  `/`,
+  `/list`,
   async (req, res, next) => {
     try {
       // verify that your data is making it here to the API by using console.log();
       // call the AssessmentService.getList function from packages/api/src/microservices/Assessment-Service.js
-      const assessments = [];
-
+      const assessments = await AssessmentService.getList();
       ResponseHandler(
         res,
         `Fetched assessments`,
